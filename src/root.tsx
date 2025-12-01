569586a30032fd570a7906c78f3766a5dec033f5
Antonio Cheong <acheong@duti.dev>
2025-12-01 14:23:39 +0000

    fix race condition and have ratings in wishlist

--- src/root.tsx
+++ src/root.tsx
@@ -15,9 +15,12 @@ import { ProfilePage } from "$pages/profile.tsx";
 import { SearchPage } from "$pages/searchpage.tsx";
 import { WishListPage } from "$pages/wishlist.tsx";
 import { getCurrentUser } from "$utils/auth.ts";
+import { initialize } from "./framework/init.ts";
 import { RegisterPage } from "./pages/register.tsx";
 
 export async function Root(props: { request: Request }) {
+	// Initialize just in case
+	await initialize();
 	const user = getCurrentUser(props.request);
 
 	const sortOptions = await getAllSortOptions();
